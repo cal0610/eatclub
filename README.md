b8a826e91fd8f810ddd77404a091fa80f39da3df
Calvin Pang <cal.pang123@gmail.com>
2025-07-12 09:47:23 +1000

    initial template commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Eat club technical round 2 challenge
+
+# Development
+
+# Recommended improvements
---
+++ src/test/java/com/eatclub/roundtwo/challenge/SpringAppTests.java
@@ -0,0 +1,13 @@
+package com.eatclub.roundtwo.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
